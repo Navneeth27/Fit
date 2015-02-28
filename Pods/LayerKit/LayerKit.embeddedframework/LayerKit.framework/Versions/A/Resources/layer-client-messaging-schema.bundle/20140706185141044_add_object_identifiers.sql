eb4c20d15f31c054d41ce774680d075b56881adb
bharv410 <benjamin.harvey@globant.com>
2015-02-28 10:41:06 -0800

    do pod install after pulling this repo

--- Pods/LayerKit/LayerKit.embeddedframework/LayerKit.framework/Versions/A/Resources/layer-client-messaging-schema.bundle/20140706185141044_add_object_identifiers.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE conversations ADD COLUMN object_identifier TEXT;
-ALTER TABLE messages ADD COLUMN object_identifier TEXT;
--- Pods/Target Support Files/Pods-Fitovate/Pods-Fitovate-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Fitovate : NSObject
-@end
-@implementation PodsDummy_Pods_Fitovate
-@end
